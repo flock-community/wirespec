05843e9039777f182517c0575e298091892645fc
Willem Veelenturf <willem.veelenturf@gmail.com>
2022-09-28 22:29:20 +0200

    Refactor intellij plugin

--- lsp/intellij-plugin/src/main/kotlin/Element.kt
+++
@@ -1,6 +0,0 @@
-package community.flock.wirespec.lsp.intellij_plugin
-
-import com.intellij.extapi.psi.ASTWrapperPsiElement
-import com.intellij.lang.ASTNode
-
-class Element(ast: ASTNode) : ASTWrapperPsiElement(ast)
--- lsp/intellij-plugin/src/main/kotlin/TokenSets.kt
+++
@@ -1,7 +0,0 @@
-package community.flock.wirespec.lsp.intellij_plugin
-
-import com.intellij.psi.tree.TokenSet as IntellijTokenSet
-
-object TokenSets {
-    var CUSTOM_TYPE = IntellijTokenSet.create(Types.CUSTOM_TYPE)
-}
