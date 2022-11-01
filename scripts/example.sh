d3b016afe84940c346fa3ed328e929752adc3018
Willem Veelenturf <willem.veelenturf@gmail.com>
2022-11-01 14:23:36 +0100

    Fix scripts

--- scripts/example.sh
+++ scripts/example.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+dir="$(dirname -- "$0")"
+
 # publish and build gradle example
 ./gradlew plugin:maven:publishToMavenLocal
-(cd examples/spring-boot-gradle-plugin; ./gradlew wirespec build)
+(cd "$dir"/../examples/spring-boot-gradle-plugin; ./gradlew wirespec build)
 
 # publish and build maven example
 ./gradlew plugin:gradle:publishToMavenLocal
-(cd examples/spring-boot-maven-plugin; ./mvnw package)
+(cd "$dir"/../examples/spring-boot-maven-plugin; ./mvnw package)
