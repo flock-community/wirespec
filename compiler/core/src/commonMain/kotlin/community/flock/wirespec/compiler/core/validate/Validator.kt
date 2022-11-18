003b2dfc624c6386376474740b75b570234e7c4e
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2022-11-18 14:32:02 +0100

    refactor: optimize and validate placeholders (#44)

--- compiler/core/src/commonMain/kotlin/community/flock/wirespec/compiler/core/validate/Validator.kt
+++ compiler/core/src/commonMain/kotlin/community/flock/wirespec/compiler/core/validate/Validator.kt
@@ -1,8 +1,5 @@
 package community.flock.wirespec.compiler.core.validate
 
-import community.flock.wirespec.compiler.core.Either
-import community.flock.wirespec.compiler.core.either
-import community.flock.wirespec.compiler.core.exceptions.WirespecException.CompilerException
 import community.flock.wirespec.compiler.core.parse.AST
 
-fun AST.validate(): Either<CompilerException, AST> = either { this }
+fun AST.validate() = this
