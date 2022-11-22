2dad9087a0c424aa4c5b2095dc0105e620ce34f4
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2022-11-22 10:33:12 +0100

    fix: whitespace filter in parser (#45)

--- compiler/core/src/commonMain/kotlin/community/flock/wirespec/compiler/core/optimize/Optimizer.kt
+++ compiler/core/src/commonMain/kotlin/community/flock/wirespec/compiler/core/optimize/Optimizer.kt
@@ -1,6 +1,5 @@
 package community.flock.wirespec.compiler.core.optimize
 
 import community.flock.wirespec.compiler.core.tokenize.Token
-import community.flock.wirespec.compiler.core.tokenize.types.WhiteSpace
 
-fun List<Token>.optimize() = filterNot { it.type is WhiteSpace }
+fun List<Token>.optimize() = this
