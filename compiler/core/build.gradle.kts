366e1a92308f4fbade56b05f41261a855d629234
Willem Veelenturf <willem.veelenturf@gmail.com>
2022-11-30 20:27:57 +0100

    PIPELINE: publish lib jvm

--- compiler/core/build.gradle.kts
+++ compiler/core/build.gradle.kts
@@ -33,3 +33,22 @@ kotlin {
         }
     }
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("wirespec") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            credentials {
+                username = System.getenv("JFROG_USERNAME")
+                password = System.getenv("JFROG_TOKEN")
+            }
+            name = "flock-maven"
+            url = uri("https://flock.jfrog.io/artifactory/flock-maven")
+        }
+    }
+}
