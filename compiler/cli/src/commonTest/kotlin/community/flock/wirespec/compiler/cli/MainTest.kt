22e98f9b673953a43b04e227ce40d21e42ace243
Willem Veelenturf <willem.veelenturf@gmail.com>
2023-06-07 19:48:39 +0200

    Improve test case

--- compiler/cli/src/commonTest/kotlin/community/flock/wirespec/compiler/cli/MainTest.kt
+++ compiler/cli/src/commonTest/kotlin/community/flock/wirespec/compiler/cli/MainTest.kt
@@ -1,15 +1,21 @@
 package community.flock.wirespec.compiler.cli
 
-import community.flock.wirespec.compiler.cli.io.Directory
-import kotlin.random.Random
-import kotlin.test.Ignore
+import community.flock.wirespec.compiler.cli.io.Extension
+import community.flock.wirespec.compiler.cli.io.File
+import community.flock.wirespec.compiler.cli.io.FullFilePath
+import community.flock.wirespec.compiler.core.emit.common.DEFAULT_PACKAGE_NAME
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 
 class CliTest {
 
     fun outputDir() = "/tmp/${getRandomString(8)}"
     val inputDir = "src/commonTest/resources"
+
+    class TestFile(val directory: String, val fileName: String, val extension: Extension) : File(FullFilePath(directory, fileName, extension)) {
+        override fun copy(fileName: String) = TestFile(directory, fileName, extension)
+    }
 
     @Test
     fun testMainHelp() {
@@ -18,15 +24,45 @@ class CliTest {
 
     @Test
     fun testMainOutput() {
-        main(arrayOf(inputDir, "-o", outputDir()))
+        val packageDir = DEFAULT_PACKAGE_NAME.replace(".", "/")
+        val outputDir = outputDir()
+        main(arrayOf(inputDir, "-o", outputDir))
+
+        val kotlin = TestFile("$outputDir/$packageDir", "Bla", Extension.Kotlin).read()
+
+        val expected = """
+            package community.flock.wirespec.generated
+
+            data class Bla(
+              val yolo: String
+            )
+            
+        """.trimIndent()
+        assertEquals(expected, kotlin)
     }
 
     @Test
     fun testMainJavaPackage() {
-        main(arrayOf(inputDir, "-o", outputDir(), "-l",  "Kotlin", "-l",  "Java", "-p", "community.flock.next"))
+        val packageName = "community.flock.next"
+        val packageDir = packageName.replace(".", "/")
+        val outputDir = outputDir()
+        main(arrayOf(inputDir, "-o", outputDir, "-l", "Kotlin", "-l", "Java", "-p", "community.flock.next"))
+
+        val java = TestFile("$outputDir/$packageDir", "Bla", Extension.Java).read()
+
+        val expected = """
+            package community.flock.next;
+
+            public record Bla(
+              String yolo
+            ) {};
+            
+            
+        """.trimIndent()
+        assertEquals(expected, java)
     }
 
-    private fun getRandomString(length: Int) : String {
+    private fun getRandomString(length: Int): String {
         val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
         return (1..length)
             .map { allowedChars.random() }
