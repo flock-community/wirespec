781bc9373860c920edec441e226b4b2678a350ad
Willem Veelenturf <willem.veelenturf@gmail.com>
2023-07-29 22:12:30 +0200

    Fix test

--- compiler/core/src/commonTest/kotlin/community/flock/wirespec/compiler/common/Assert.kt
+++ compiler/core/src/commonTest/kotlin/community/flock/wirespec/compiler/common/Assert.kt
@@ -6,14 +6,14 @@ import community.flock.wirespec.compiler.core.exceptions.WirespecException
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-fun Either<Nel<WirespecException.CompilerException>, String>.assertValid(expected: String) {
+fun Either<Nel<WirespecException>, String>.assertValid(expected: String) {
     when (this) {
         is Either.Left -> error("Valid expected")
         is Either.Right -> assertEquals(expected, value)
     }
 }
 
-fun <B> Either<Nel<WirespecException.CompilerException>, B>.assertInvalid(expected: String) {
+fun <B> Either<Nel<WirespecException>, B>.assertInvalid(expected: String) {
     when (this) {
         is Either.Left -> assertTrue(value.map { it.message }.contains(expected))
         is Either.Right -> error("Invalid expected")
