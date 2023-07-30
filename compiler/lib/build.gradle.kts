f04a5cc28ced6303cfe8edf8ab0a16e1c2e69f9e
Willem Veelenturf <willem.veelenturf@gmail.com>
2023-07-30 21:26:29 +0200

    Publish npm

--- compiler/lib/build.gradle.kts
+++ compiler/lib/build.gradle.kts
@@ -15,11 +15,16 @@ kotlin {
     js(IR) {
         nodejs()
         binaries.library()
+        compilations["main"].packageJson {
+            customField("name", "@flock/wirespec")
+            customField("bin", mapOf("wirespec" to "kotlin/wirespec-bin.js"))
+        }
     }
 
     sourceSets {
         val commonMain by getting {
             dependencies {
+                implementation(project(":compiler:cli"))
                 implementation(project(":compiler:core"))
             }
         }
