e14bef60ee390622d08ee209a74f0b57529b3ced
Willem Veelenturf <willem.veelenturf@ing.com>
2023-11-05 15:02:46 +0100

    Update to java 21

--- src/compiler/core/build.gradle.kts
+++ src/compiler/core/build.gradle.kts
@@ -25,7 +25,7 @@ kotlin {
         withJava()
         java {
             toolchain {
-                languageVersion.set(JavaLanguageVersion.of(17))
+                languageVersion.set(JavaLanguageVersion.of(21))
             }
         }
     }
