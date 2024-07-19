675a0d2e42b9080aa0974c77be39e62b9bfd0bc2
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2024-07-19 12:44:39 +0200

    chore: examples to kotlin 2.0.0

--- examples/wirespec-gradle-plugin-ktor/build.gradle.kts
+++ examples/wirespec-gradle-plugin-ktor/build.gradle.kts
@@ -16,7 +16,7 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    kotlin("jvm") version "1.9.24"
+    kotlin("jvm") version "2.0.0"
     kotlin("plugin.serialization") version "2.0.0-RC3"
     id("io.ktor.plugin") version "2.3.9"
     id("community.flock.wirespec.plugin.gradle") version "0.0.0-SNAPSHOT"
