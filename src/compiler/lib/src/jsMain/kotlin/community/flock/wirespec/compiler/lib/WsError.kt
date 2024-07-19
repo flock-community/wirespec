9b6fb95fad6e7ba90136a4e90f7115aa2b7830bc
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2024-07-19 14:31:28 +0200

    feat: js error line and position information

--- src/compiler/lib/src/jsMain/kotlin/community/flock/wirespec/compiler/lib/WsError.kt
+++ src/compiler/lib/src/jsMain/kotlin/community/flock/wirespec/compiler/lib/WsError.kt
@@ -6,12 +6,16 @@ import community.flock.wirespec.compiler.core.exceptions.WirespecException
 
 @JsExport
 data class WsError(
+    @JsName("line") val line: Int,
+    @JsName("position") val position: Int,
     @JsName("index") val index: Int,
     @JsName("length") val length: Int,
     @JsName("value") val value: String
 )
 
 fun WirespecException.produce() = WsError(
+    line = coordinates.line,
+    position = coordinates.position,
     index = coordinates.idxAndLength.idx - coordinates.idxAndLength.length,
     length = coordinates.idxAndLength.length,
     value = message ?: "No message"
