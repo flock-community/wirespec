901eee321092b9e354a4c75e0746f6e7b0155e3e
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2024-08-08 22:19:17 +0200

    refactor: logger with loglevel

--- src/compiler/core/src/commonMain/kotlin/community/flock/wirespec/compiler/utils/Logger.kt
+++ src/compiler/core/src/commonMain/kotlin/community/flock/wirespec/compiler/utils/Logger.kt
@@ -1,18 +1,40 @@
 package community.flock.wirespec.compiler.utils
 
-open class Logger(private val enableLogging: Boolean = false) {
+import community.flock.wirespec.compiler.utils.Logger.Level.DEBUG
+import community.flock.wirespec.compiler.utils.Logger.Level.ERROR
+import community.flock.wirespec.compiler.utils.Logger.Level.INFO
+import community.flock.wirespec.compiler.utils.Logger.Level.WARN
 
-    open fun debug(s: String) = if (enableLogging) println(s) else Unit
+open class Logger(private val logLevel: Level? = ERROR) {
 
-    open fun info(s: String) = if (enableLogging) println(s) else Unit
+    open fun debug(s: String) = when (logLevel) {
+        null -> Unit
+        DEBUG, INFO, WARN, ERROR -> println(s)
+    }
 
-    open fun warn(s: String) = println(s)
+    open fun info(s: String) = when (logLevel) {
+        null, DEBUG -> Unit
+        INFO, WARN, ERROR -> println(s)
+    }
 
-    fun info(s: String, block: () -> String) = run {
-        info(s)
-        block()
+    open fun warn(s: String) = when (logLevel) {
+        null, DEBUG, INFO -> Unit
+        WARN, ERROR -> println(s)
+    }
+
+    open fun error(s: String) = when (logLevel) {
+        null, DEBUG, INFO, WARN -> Unit
+        ERROR -> println(s)
+    }
+
+    enum class Level {
+        DEBUG, INFO, WARN, ERROR;
+
+        companion object {
+            override fun toString() = entries.joinToString(", ")
+        }
     }
 
 }
 
-val noLogger = object : Logger(false) {}
+val noLogger = object : Logger(logLevel = null) {}
