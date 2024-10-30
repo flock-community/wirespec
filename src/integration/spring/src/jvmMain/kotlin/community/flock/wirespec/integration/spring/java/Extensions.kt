d2a8e56a84ab9c9637cdcb04b86666d11a179706
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2024-10-30 08:57:41 +0100

    chore: cleanup

--- src/integration/spring/src/jvmMain/kotlin/community/flock/wirespec/integration/spring/java/Extensions.kt
+++ src/integration/spring/src/jvmMain/kotlin/community/flock/wirespec/integration/spring/java/Extensions.kt
@@ -6,27 +6,18 @@ import java.lang.reflect.Method
 import java.util.function.Function
 import kotlin.reflect.full.companionObjectInstance
 
-fun Class<*>.getStaticClass(): Class<*> =
-    if (isKotlinClass()) {
-        kotlin.companionObjectInstance?.javaClass ?: error("not found")
-    } else {
-        this
-    }
-
-fun Class<*>.getStaticMethod(name: String): Method? = getStaticClass().methods.find { it.name == name }
-
 fun Class<*>.invoke(
     method: Method,
     wirespecSerialization: Wirespec.Serialization<String>,
     request: Wirespec.Request<BufferedReader>
 ): Wirespec.Request<*> = if (isKotlinClass()) {
-    val func = method.invoke(
+    val func = method(
         kotlin.companionObjectInstance,
         wirespecSerialization
     ) as (Wirespec.Request<BufferedReader>) -> Wirespec.Request<*>
     func(request)
 } else {
-    val func = method.invoke(
+    val func = method(
         this,
         wirespecSerialization
     ) as Function<Wirespec.Request<BufferedReader>, Wirespec.Request<*>>
