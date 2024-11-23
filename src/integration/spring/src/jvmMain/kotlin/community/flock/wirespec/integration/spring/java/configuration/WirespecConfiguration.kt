c72287c873992dd136f2ba2ca426b4303849c969
Niels Simonides <5821618+nsmnds@users.noreply.github.com>
2024-11-23 10:23:50 +0100

    Fixed argument resolver & finer grained serialization (#298)
    Co-authored-by: nsimonides <nsimonides@bol.com>

--- src/integration/spring/src/jvmMain/kotlin/community/flock/wirespec/integration/spring/java/configuration/WirespecConfiguration.kt
+++ src/integration/spring/src/jvmMain/kotlin/community/flock/wirespec/integration/spring/java/configuration/WirespecConfiguration.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import community.flock.wirespec.integration.jackson.java.WirespecModuleJava
 import community.flock.wirespec.integration.spring.java.web.WirespecResponseBodyAdvice
 import community.flock.wirespec.java.Wirespec
+import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -17,12 +18,35 @@ open class WirespecConfiguration {
     open fun wirespecSerialization(objectMapper: ObjectMapper) = object : Wirespec.Serialization<String> {
 
         private val wirespecObjectMapper = objectMapper.copy().registerModule(WirespecModuleJava())
+        private val stringListDelimiter = ","
 
-        override fun <T> serialize(body: T, type: Type?): String = wirespecObjectMapper.writeValueAsString(body)
+        override fun <T> serialize(body: T, type: Type?): String = when {
+            body is String -> body
+            isStringIterable(type) && body is Iterable<*> -> body.joinToString(stringListDelimiter)
+            else -> wirespecObjectMapper.writeValueAsString(body)
+        }
 
         override fun <T : Any> deserialize(raw: String?, valueType: Type?): T? = raw?.let {
-            val type = wirespecObjectMapper.constructType(valueType)
-            wirespecObjectMapper.readValue(it, type)
+            when {
+                isStringIterable(valueType) -> {
+                    @Suppress("UNCHECKED_CAST")
+                    raw.split(stringListDelimiter) as T
+                }
+                else -> {
+                    val type = wirespecObjectMapper.constructType(valueType)
+                    wirespecObjectMapper.readValue(it, type)
+                }
+            }
+        }
+
+        private fun isStringIterable(type: Type?): Boolean {
+            if (type !is ParameterizedType) return false
+
+            val rawType = type.rawType as Class<*>
+            if (!Iterable::class.java.isAssignableFrom(rawType)) return false
+
+            val typeArgument = type.actualTypeArguments.firstOrNull()
+            return typeArgument == String::class.java
         }
     }
 }
