4888d2b0dd263755927ce86055027b7b1a019828
nsimonides <nsimonides@bol.com>
2024-12-05 10:25:53 +0100

    Fixed build

--- scripts/example.sh
+++ scripts/example.sh
@@ -4,6 +4,7 @@ dir="$(dirname -- "$0")"
   src:converter:openapi:jvmJar \
   src:plugin:arguments:jvmJar \
   src:integration:jackson:publishToMavenLocal \
+  src:integration:wirespec:publishToMavenLocal \
   src:integration:spring:publishToMavenLocal \
   src:plugin:gradle:publishToMavenLocal \
   src:plugin:maven:publishToMavenLocal \
