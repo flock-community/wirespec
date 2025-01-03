c51e16dca5b0745cd8d821ff773458a8c938e1ee
nsimonides <nsimonides@bol.com>
2025-01-03 17:06:53 +0100

    Java implementation

--- src/integration/spring/src/jvmMain/kotlin/community/flock/wirespec/integration/spring/shared/Controller.kt
+++ src/integration/spring/src/jvmMain/kotlin/community/flock/wirespec/integration/spring/shared/Controller.kt
@@ -1,6 +1,8 @@
 package community.flock.wirespec.integration.spring.shared
 
 import jakarta.servlet.http.HttpServletRequest
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 
 /**
  * Depending on how the controller is mapped:
@@ -15,15 +17,21 @@ fun HttpServletRequest.extractPath() = (pathInfo ?: servletPath)
     .split("/")
     .filter { it.isNotEmpty() }
 
-fun HttpServletRequest.extractQueries() = queryString
-    ?.let { query ->
-        query.split("&").flatMap { param ->
-            val (key, value) = param.split("=", limit = 2)
-            value.split(",").map { key to it }
+fun HttpServletRequest.extractQueries() = extractQueries(queryString)
+
+fun extractQueries(queryString: String?): Map<String, List<String>> =
+    queryString
+        ?.let { query ->
+            query.split("&").flatMap { param ->
+                val (key, value) = param.split("=", limit = 2)
+                val decodedKey = URLDecoder.decode(key, StandardCharsets.UTF_8)
+                value.split(",").map {
+                    decodedKey to URLDecoder.decode(it, StandardCharsets.UTF_8)
+                }
+            }
+                .groupBy(
+                    keySelector = { it.first },
+                    valueTransform = { it.second }
+                )
         }
-            .groupBy(
-                keySelector = { it.first },
-                valueTransform = { it.second }
-            )
-    }
-    .orEmpty()
+        .orEmpty()
