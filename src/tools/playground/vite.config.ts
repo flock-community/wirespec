b02f0c612664b9903732dc80dec8de9856ec4dfd
Willem Veelenturf <willem.veelenturf@ing.com>
2025-01-24 15:11:09 +0100

    Add gradle config

--- src/tools/playground/vite.config.ts
+++ src/tools/playground/vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      react()
-  ],
+    plugins: [
+        react()
+    ],
     optimizeDeps: {
         include: ['wirespec'], // Add the package containing this namespace
     },
