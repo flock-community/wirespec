bf7c696e4846b451a2f8a677a8c6ad47d9f664ec
Julius van Dis <julius.van.dis@flock.community>
2025-02-07 18:24:54 +0100

    Ensure to release artifacts from all modules, unless explicitly excluded
    Following a blacklist model to exclude artifacts from being published to maven central, seems like a more sensible thing, then whitelisting, at least for the moment. Seeing we've been missing the publication for almost all artifacts added in the past year: spring integration, cli arguments, integration wirespec, converter-avro, integration avro.
    
    To test this feature, I recommend to locally delete all artifacts of wirespec:
    
    ```bash
    rm -rfv ~/.m2/repository/community/flock/wirespec/
    ```
    and then run `./scripts/example.sh` to see if all necessary artifacts are generated.

--- scripts/example.sh
+++ scripts/example.sh
@@ -1,16 +1,7 @@
 dir="$(dirname -- "$0")"
 
 ./gradlew \
-  src:converter:openapi:jvmJar \
-  src:plugin:arguments:jvmJar \
-  src:converter:avro:jvmJar \
-  src:converter:avro:publishToMavenLocal \
-  src:integration:avro:publishToMavenLocal \
-  src:integration:jackson:publishToMavenLocal \
-  src:integration:wirespec:publishToMavenLocal \
-  src:integration:spring:publishToMavenLocal \
-  src:plugin:gradle:publishToMavenLocal \
-  src:plugin:maven:publishToMavenLocal \
+  publishToMavenLocal \
   src:plugin:npm:jsNodeProductionLibraryDistribution &&
 (cd "$dir"/../src/ide/vscode && npm i && npm run build) &&
-(cd "$dir"/../examples && make clean && make build)
+(cd "$dir"/../examples && make clean build)
