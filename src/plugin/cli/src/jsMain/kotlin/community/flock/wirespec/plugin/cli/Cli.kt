2825df355f1685d8ddd8b0675fd3aafb85e55490
Willem Veelenturf <willem.veelenturf@ing.com>
2025-02-20 07:32:13 +0100

    Restore Cli

---
+++ src/plugin/cli/src/jsMain/kotlin/community/flock/wirespec/plugin/cli/Cli.kt
@@ -0,0 +1,25 @@
+@file:OptIn(ExperimentalJsExport::class)
+
+package community.flock.wirespec.plugin.cli
+
+import community.flock.wirespec.compiler.core.ParseContext
+import community.flock.wirespec.compiler.core.parse
+import community.flock.wirespec.compiler.core.parse.Definition
+import community.flock.wirespec.compiler.lib.WsNode
+import community.flock.wirespec.compiler.lib.produce
+import community.flock.wirespec.compiler.utils.noLogger
+
+@JsExport
+fun cli(args: Array<String>) {
+    main(args)
+}
+
+@JsExport
+fun parser(source: String): Array<WsNode> =
+    object : ParseContext {
+        override val logger = noLogger
+    }.parse(source).getOrNull()
+        ?.filterIsInstance<Definition>()
+        ?.map { it.produce() }
+        ?.toTypedArray()
+        ?: error("Cannot parse source")
