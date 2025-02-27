4c272b8167191dc429462e8d99e8faf863a4080c
Willem Veelenturf <willem.veelenturf@ing.com>
2025-02-27 10:20:31 +0100

    Add -jvm in bom

--- src/bom/build.gradle.kts
+++ src/bom/build.gradle.kts
@@ -4,3 +4,15 @@ plugins {
 
 group = "${libs.versions.group.id.get()}"
 version = System.getenv(libs.versions.from.env.get()) ?: libs.versions.default.get()
+
+bomGenerator {
+    includeDependency("$group.compiler", "core-jvm", "$version")
+    includeDependency("$group.compiler", "lib-jvm", "$version")
+    includeDependency("$group.converter", "avro-jvm", "$version")
+    includeDependency("$group.converter", "openapi-jvm", "$version")
+    includeDependency("$group.integration", "avro-jvm", "$version")
+    includeDependency("$group.integration", "jackson-jvm", "$version")
+    includeDependency("$group.integration", "spring-jvm", "$version")
+    includeDependency("$group.integration", "wirespec-jvm", "$version")
+
+}
