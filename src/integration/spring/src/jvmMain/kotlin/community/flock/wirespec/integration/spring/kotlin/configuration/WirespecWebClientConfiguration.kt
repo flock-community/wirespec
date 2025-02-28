e0b635b5d1816f862bbd820ae494128a2ca32770
Yvonne Haagsma <yvonne.haagsma@flock.community>
2025-02-28 13:52:41 +0100

    Add property (auto)configuration for WebClient base url

--- src/integration/spring/src/jvmMain/kotlin/community/flock/wirespec/integration/spring/kotlin/configuration/WirespecWebClientConfiguration.kt
+++ src/integration/spring/src/jvmMain/kotlin/community/flock/wirespec/integration/spring/kotlin/configuration/WirespecWebClientConfiguration.kt
@@ -3,6 +3,7 @@ package community.flock.wirespec.integration.spring.kotlin.configuration
 import community.flock.wirespec.integration.spring.kotlin.client.WirespecWebClient
 import community.flock.wirespec.kotlin.Wirespec
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
@@ -14,14 +15,20 @@ import org.springframework.web.reactive.function.client.WebClient
 open class WirespecWebClientConfiguration(
     val serialization: Wirespec.Serialization<String>,
 ) {
+    @Value("\${wirespec.spring.webclient.baseUrl:http://localhost:8080}")
+    lateinit var webClientBaseUrl: String
+
     @Bean("wirespecSpringWebClient")
     @ConditionalOnMissingBean(name = ["wirespecSpringWebClient"])
-    open fun defaultWebClient(): WebClient = WebClient.builder().build()
+    open fun defaultWebClient(): WebClient = WebClient.builder().baseUrl(webClientBaseUrl).build()
 
     @Bean
     @ConditionalOnMissingBean(WirespecWebClient::class)
-    open fun wirespecWebClient(@Qualifier("wirespecSpringWebClient") webClient: WebClient): WirespecWebClient = WirespecWebClient(
-        client = webClient,
-        wirespecSerde = serialization,
-    )
+    open fun wirespecWebClient(
+        @Qualifier("wirespecSpringWebClient") webClient: WebClient,
+    ): WirespecWebClient =
+        WirespecWebClient(
+            client = webClient,
+            wirespecSerde = serialization,
+        )
 }
