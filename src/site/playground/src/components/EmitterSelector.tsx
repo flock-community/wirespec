97abf4744f190a1bdd58e1dcfd07a47eeb019136
Tom Westerhuis <tom.westerhuis@flock.community>
2025-03-14 14:49:07 +0100

    dynamic selectors

--- src/site/playground/src/components/EmitterSelector.tsx
+++ src/site/playground/src/components/EmitterSelector.tsx
@@ -1,9 +1,13 @@
 import { FormControl, MenuItem, Select } from "@mui/material";
 import { useNavigate, useSearch } from "@tanstack/react-router";
-import { type Emitter } from "../routes/index";
+import {
+  type CompilerEmitter,
+  type ConverterEmitter,
+  type Emitter,
+} from "../routes/index";
 
 export function EmitterSelector() {
-  const options: { value: Emitter; label: string }[] = [
+  const compilerOptions: { value: CompilerEmitter; label: string }[] = [
     { value: "typescript", label: "TypeScript" },
     { value: "kotlin", label: "Kotlin" },
     { value: "scala", label: "Scala" },
@@ -12,22 +16,31 @@ export function EmitterSelector() {
     { value: "open_api_v3", label: "OpenAPI v3" },
     { value: "avro", label: "Avro" },
   ];
-
+  const converterOption: { value: ConverterEmitter; label: string } = {
+    value: "wirespec",
+    label: "Wirespec",
+  };
   const navigate = useNavigate({ from: "/" });
   const { emitter, specification } = useSearch({ from: "/" });
-
-  const handleChange = (event: { target: { value: string } }) => {
+  const handleChange = (event: { target: { value: string } }) =>
     navigate({
       search: () => ({ emitter: event.target.value as Emitter, specification }),
     });
-  };
 
   return (
     <FormControl sx={{ minWidth: 120 }} size="small">
       <Select value={emitter} onChange={handleChange}>
-        {options.map((option) => (
-          <MenuItem value={option.value}>{option.label}</MenuItem>
-        ))}
+        {specification === "wirespec" ? (
+          compilerOptions.map(({ value, label }) => (
+            <MenuItem key={value} value={value}>
+              {label}
+            </MenuItem>
+          ))
+        ) : (
+          <MenuItem key={converterOption.value} value={converterOption.value}>
+            {converterOption.label}
+          </MenuItem>
+        )}
       </Select>
     </FormControl>
   );
--- src/site/playground/src/components/SpecificationSelector.tsx
+++ src/site/playground/src/components/SpecificationSelector.tsx
@@ -5,22 +5,19 @@ import { type Specification } from "../routes/index";
 type CompileOption = { value: Specification; label: string };
 
 export function SpecificationSelector() {
-  const compileOptions: CompileOption[] = [
-    { value: "wirespec", label: "Wirespec" },
-  ];
-
+  const compileOption: CompileOption = { value: "wirespec", label: "Wirespec" };
   const convertOptions: CompileOption[] = [
     { value: "open_api_v2", label: "Open API v2" },
     { value: "open_api_v3", label: "Open API v3" },
   ];
 
   const navigate = useNavigate({ from: "/" });
-  const { specification, emitter } = useSearch({ from: "/" });
+  const { specification } = useSearch({ from: "/" });
 
   const handleChange = (event: { target: { value: string } }) => {
     if (event.target.value === "wirespec") {
       return navigate({
-        search: () => ({ specification: "wirespec", emitter }),
+        search: () => ({ specification: "wirespec", emitter: "typescript" }),
       });
     }
 
@@ -36,13 +33,12 @@ export function SpecificationSelector() {
     <FormControl sx={{ minWidth: 120 }} size="small">
       <Select value={specification} onChange={handleChange}>
         <ListSubheader>Compile options</ListSubheader>
-        {compileOptions.map((option) => (
-          <MenuItem value={option.value}>{option.label}</MenuItem>
-        ))}
-
+        <MenuItem value={compileOption.value}>{compileOption.label}</MenuItem>
         <ListSubheader>Convert options</ListSubheader>
-        {convertOptions.map((option) => (
-          <MenuItem value={option.value}>{option.label}</MenuItem>
+        {convertOptions.map(({ value, label }) => (
+          <MenuItem key={value} value={value}>
+            {label}
+          </MenuItem>
         ))}
       </Select>
     </FormControl>
