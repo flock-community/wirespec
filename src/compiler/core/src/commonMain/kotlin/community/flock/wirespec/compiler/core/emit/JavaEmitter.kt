f17e4a74f680e56bd20783728e29509b4874ea1f
Willem Veelenturf <willem.veelenturf@ing.com>
2025-03-25 12:19:33 +0100

    Optimize

--- src/compiler/core/src/commonMain/kotlin/community/flock/wirespec/compiler/core/emit/JavaEmitter.kt
+++ src/compiler/core/src/commonMain/kotlin/community/flock/wirespec/compiler/core/emit/JavaEmitter.kt
@@ -84,38 +84,25 @@ open class JavaEmitter(
 
     override fun Field.emit() = "${reference.emit()} ${emit(identifier)}"
 
-    override fun Reference.emit(): String = emitType().let { if (isNullable) "java.util.Optional<$it>" else it }
+    override fun Reference.emit(): String = emitType()
+        .let { if (isNullable) "java.util.Optional<$it>" else it }
 
-    private fun Reference.emitType(void: String = "void"): String = when (this) {
+    private fun Reference.emitType(): String = when (this) {
         is Reference.Dict -> "java.util.Map<String, ${reference.emit()}>"
         is Reference.Iterable -> "java.util.List<${reference.emit()}>"
         is Reference.Unit -> "void"
         is Reference.Any -> "Object"
         is Reference.Custom -> value
-        is Reference.Primitive -> when (type) {
-            is Reference.Primitive.Type.String -> "String"
-            is Reference.Primitive.Type.Integer -> when (type.precision) {
-                Reference.Primitive.Type.Precision.P32 -> "Integer"
-                Reference.Primitive.Type.Precision.P64 -> "Long"
-            }
-
-            is Reference.Primitive.Type.Number -> when (type.precision) {
-                Reference.Primitive.Type.Precision.P32 -> "Float"
-                Reference.Primitive.Type.Precision.P64 -> "Double"
-            }
-
-            is Reference.Primitive.Type.Boolean -> "Boolean"
-            is Reference.Primitive.Type.Bytes -> "byte[]"
-        }
+        is Reference.Primitive -> emit()
     }
 
     fun Reference.emitRoot(void: String = "void"): String = when (this) {
         is Reference.Dict -> reference.emitRoot()
         is Reference.Iterable -> reference.emitRoot()
         is Reference.Unit -> void
-        is Reference.Any -> "Object"
-        is Reference.Custom -> emit()
-        is Reference.Primitive -> emit()
+        is Reference.Any -> emitType()
+        is Reference.Custom -> emitType()
+        is Reference.Primitive -> emitType()
     }
 
     private fun Reference.Custom.emit() = value
