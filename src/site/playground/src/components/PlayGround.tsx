0ece99dc4895a71ef4e591164d2f2dcae85e0d52
Vincent de Bruijn <vincent.de.bruijn@ing.com>
2025-03-28 13:40:24 +0100

    fix: scale font-size of Monaco editor with screen width

--- src/site/playground/src/components/PlayGround.tsx
+++ src/site/playground/src/components/PlayGround.tsx
@@ -1,5 +1,6 @@
 import Editor from "@monaco-editor/react";
 import { Language } from "../routes";
+import {useEffect, useState} from "react";
 
 interface PlayGroundProps {
   code: string;
@@ -8,12 +9,23 @@ interface PlayGroundProps {
 }
 
 export function PlayGround({ code, setCode, language }: PlayGroundProps) {
-  return (
+    const [width, setWidth] = useState(0);
+    useEffect(() => {
+        const updateWindowDimensions = () => {
+            const newWidth = window.innerWidth;
+            setWidth(newWidth);
+        };
+
+        window.addEventListener("resize", updateWindowDimensions);
+
+        return () => window.removeEventListener("resize", updateWindowDimensions)
+    }, []);
+    return (
     <Editor
       language={language}
       theme="vs-dark"
       height="100vh"
-      options={{ minimap: { enabled: false } }}
+      options={{ minimap: { enabled: false }, fontSize: width < 768 ? 12 : 0.01*width }}
       value={code}
       onChange={
         setCode ? (code: string | undefined) => setCode(code ?? "") : undefined
