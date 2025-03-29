1da171c9eab3600761fffae47a59106aad94de3f
Tom Westerhuis <tom.westerhuis@flock.community>
2025-03-29 20:57:43 +0100

    fix mobile case

--- src/site/playground/src/components/PlayGround.tsx
+++ src/site/playground/src/components/PlayGround.tsx
@@ -1,5 +1,6 @@
 import Editor from "@monaco-editor/react";
 import { Language } from "../routes";
+import { Box } from "@mui/material";
 
 interface PlayGroundProps {
   code: string;
@@ -9,16 +10,26 @@ interface PlayGroundProps {
 
 export function PlayGround({ code, setCode, language }: PlayGroundProps) {
   return (
-    <Editor
-      language={language}
-      theme="vs-dark"
-      height="100vh"
-      width="50vw"
-      options={{ minimap: { enabled: false } }}
-      value={code}
-      onChange={
-        setCode ? (code: string | undefined) => setCode(code ?? "") : undefined
-      }
-    />
+    <Box
+      sx={{
+        width: {
+          xs: "100vw",
+          sm: "50vw",
+        },
+      }}
+    >
+      <Editor
+        language={language}
+        theme="vs-dark"
+        height="100vh"
+        options={{ minimap: { enabled: false } }}
+        value={code}
+        onChange={
+          setCode
+            ? (code: string | undefined) => setCode(code ?? "")
+            : undefined
+        }
+      />
+    </Box>
   );
 }
