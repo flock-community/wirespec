f75436e303acb8596d16933b9cb64f02311850c9
Edwin Ouwehand <edwin.ouwehand@ing.com>
2025-04-04 15:15:02 +0200

    Clean up

--- src/compiler/core/src/commonMain/kotlin/community/flock/wirespec/compiler/core/emit/common/Emitted.kt
+++ src/compiler/core/src/commonMain/kotlin/community/flock/wirespec/compiler/core/emit/common/Emitted.kt
@@ -4,10 +4,3 @@ data class Emitted(
     val typeName: String,
     val result: String,
 )
-
-// Emitter converts the ast to a list of emitted
-
-// Emitted is content to output and what file(+ ext) to write to
-// Writer actually outputs this to a given directory
-
-// If there are several emitters, then we should output to (?)
