bac9a0b7697b15ed50c717f46e6a1f59de3decc1
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2025-04-12 15:38:00 +0200

    Dockerfile with kotlin java scala typescript compilers (#429)

--- scripts/compileTypes.sh
+++ scripts/compileTypes.sh
@@ -1,33 +1,8 @@
 #!/usr/bin/env bash
 
-dir="$(dirname -- "$0")"
+archSpecific=""
+if [[ $(uname -m) = arm64 ]]; then
+  archSpecific="--platform=linux/amd64"
+fi
 
-jvm="$dir"/../types/out/jvm
-cd "$jvm" && (cd java && javac community/flock/wirespec/generated/*.java)
-cd "$jvm" && (cd java && javac community/flock/openapi/generated/*.java)
-cd "$jvm" && (cd kotlin && kotlinc community/flock/wirespec/generated/*.kt)
-cd "$jvm" && (cd kotlin && kotlinc community/flock/openapi/generated/*.kt)
-cd "$jvm" && (cd scala && scalac community/flock/wirespec/generated/*.scala)
-cd "$jvm" && (cd scala && scalac community/flock/openapi/generated/*.scala)
-cd "$jvm" && (cd typescript && tsc --noEmit community/flock/wirespec/generated/*.ts)
-cd "$jvm" && (cd typescript && tsc --noEmit community/flock/openapi/generated/*.ts)
-
-native="$dir"/../types/out/native
-cd "$native" && (cd java && javac community/flock/wirespec/generated/*.java)
-cd "$native" && (cd java && javac community/flock/openapi/generated/*.java)
-cd "$native" && (cd kotlin && kotlinc community/flock/wirespec/generated/*.kt)
-cd "$native" && (cd kotlin && kotlinc community/flock/openapi/generated/*.kt)
-cd "$native" && (cd scala && scalac community/flock/wirespec/generated/*.scala)
-cd "$native" && (cd scala && scalac community/flock/openapi/generated/*.scala)
-cd "$native" && (cd typescript && tsc --noEmit community/flock/wirespec/generated/*.ts)
-cd "$native" && (cd typescript && tsc --noEmit community/flock/openapi/generated/*.ts)
-
-node="$dir"/../types/out/node
-cd "$node" && (cd java && javac community/flock/wirespec/generated/*.java)
-cd "$node" && (cd java && javac community/flock/openapi/generated/*.java)
-cd "$node" && (cd kotlin && kotlinc community/flock/wirespec/generated/*.kt)
-cd "$node" && (cd kotlin && kotlinc community/flock/openapi/generated/*.kt)
-cd "$node" && (cd scala && scalac community/flock/wirespec/generated/*.scala)
-cd "$node" && (cd scala && scalac community/flock/openapi/generated/*.scala)
-cd "$node" && (cd typescript && tsc --noEmit community/flock/wirespec/generated/*.ts)
-cd "$node" && (cd typescript && tsc --noEmit community/flock/openapi/generated/*.ts)
+docker run $archSpecific --rm -it -v ./types/:/app/types wirespec /app/compileTypes.sh
