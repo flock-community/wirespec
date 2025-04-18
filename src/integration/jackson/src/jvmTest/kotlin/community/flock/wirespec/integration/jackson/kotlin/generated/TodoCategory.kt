6c539c3bb280da5cd272004b9fb5896a8eda9386
Willem Veelenturf <willem.veelenturf@ing.com>
2025-04-18 18:39:57 +0200

    Fix jackson tests

---
+++ src/integration/jackson/src/jvmTest/kotlin/community/flock/wirespec/integration/jackson/kotlin/generated/TodoCategory.kt
@@ -0,0 +1,12 @@
+package community.flock.wirespec.integration.jackson.kotlin.generated
+
+import community.flock.wirespec.kotlin.Wirespec
+import kotlin.reflect.typeOf
+
+enum class TodoCategory (override val label: String): Wirespec.Enum {
+  WORK("WORK"),
+  LIFE("LIFE");
+  override fun toString(): String {
+    return label
+  }
+}
