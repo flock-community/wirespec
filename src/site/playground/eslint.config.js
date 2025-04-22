a6dab22bf6492f700e0433c08870bfc47bbb5bd7
Tom Westerhuis <49371690+martenwest@users.noreply.github.com>
2025-04-22 09:42:16 +0200

    chore: playground updates 18-04-2025 (#400)
    Co-authored-by: Tom Westerhuis <tom.westerhuis@flock.community>

---
+++ src/site/playground/eslint.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  { ignores: ["dist"] },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+]);
