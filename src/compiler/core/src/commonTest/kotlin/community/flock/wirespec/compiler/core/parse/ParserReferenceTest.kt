bf07b04ada9b9d01f1d309aaef904d05b69f4473
Edwin Ouwehand <ouwehand.edwin@gmail.com>
2025-04-22 18:13:32 +0200

    Fix comment parsing bug (#449)
    Co-authored-by: Edwin Ouwehand <edwin.ouwehand@ing.com>

--- src/compiler/core/src/commonTest/kotlin/community/flock/wirespec/compiler/core/parse/ParserReferenceTest.kt
+++ src/compiler/core/src/commonTest/kotlin/community/flock/wirespec/compiler/core/parse/ParserReferenceTest.kt
@@ -9,6 +9,7 @@ import community.flock.wirespec.compiler.utils.NoLogger
 import io.kotest.assertions.arrow.core.shouldBeLeft
 import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 import kotlin.test.Test
 
 class ParserReferenceTest {
@@ -128,4 +129,91 @@ class ParserReferenceTest {
                 first().coordinates.idxAndLength.length shouldBe 3
             }
     }
+
+    @Test
+    fun singleLine() {
+        val source = """
+            |// This is a comment
+            |type Address {
+            |  houseNumber: Integer
+            |}
+        """.trimMargin()
+
+        parser(source)
+            .shouldBeRight()
+            .apply { first().comment?.value shouldBe "This is a comment" }
+    }
+
+    @Test
+    fun multiLine() {
+        val source = """
+            |/* This is a comment */
+            |type Address {
+            |  houseNumber: Integer
+            |}
+        """.trimMargin()
+
+        parser(source)
+            .shouldBeRight()
+            .apply { first().comment?.value shouldBe "This is a comment" }
+    }
+
+    @Test
+    fun afterRegex() {
+        val source = """
+            |/* This is a comment */
+            |type Date /^([0-9]{2}-[0-9]{2}-20[0-9]{2})${'$'}/g
+            |
+            |// This is a comment too
+            |type Address {
+            |  houseNumber: Integer
+            |}
+        """.trimMargin()
+
+        parser(source)
+            .shouldBeRight()
+            .apply { first().comment?.value shouldBe "This is a comment" }
+            .apply { first().identifier.value shouldBe "Date" }
+            .apply { (first() as Refined).validator.value shouldBe "/^([0-9]{2}-[0-9]{2}-20[0-9]{2})\$/g" }
+            .apply { get(1).comment?.value shouldBe "This is a comment too" }
+            .apply { get(1).identifier.value shouldBe "Address" }
+            .apply { get(1).shouldBeInstanceOf<Type>() }
+    }
+
+    // region NOT YET SUPPORTED
+
+    @Test
+    fun loseComments() {
+        val source = "// This is a comment"
+
+        parser(source)
+            .shouldBeLeft()
+    }
+
+    @Test
+    fun fileEndingComments() {
+        val source = """
+            |type Address {
+            |  houseNumber: Integer
+            |}
+            |// This is a comment"
+        """.trimMargin()
+
+        parser(source)
+            .shouldBeLeft()
+    }
+
+    @Test
+    fun commentsInDefinitions() {
+        val source = """
+            |type Address { // This is a comment
+            |  houseNumber: Integer
+            |}
+        """.trimMargin()
+
+        parser(source)
+            .shouldBeLeft()
+    }
+
+    // endregion
 }
