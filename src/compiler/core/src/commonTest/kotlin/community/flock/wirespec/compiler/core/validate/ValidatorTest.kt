a2d36e701615cf2c8e17abfa128b8421f9ead598
Edwin Ouwehand <ouwehand.edwin@gmail.com>
2025-04-23 12:22:25 +0200

    Clean up test

--- src/compiler/core/src/commonTest/kotlin/community/flock/wirespec/compiler/core/validate/ValidatorTest.kt
+++ src/compiler/core/src/commonTest/kotlin/community/flock/wirespec/compiler/core/validate/ValidatorTest.kt
@@ -1,36 +1,54 @@
 package community.flock.wirespec.compiler.core.validate
 
-import arrow.core.nonEmptyListOf
+import arrow.core.EitherNel
+import arrow.core.toNonEmptyListOrNull
 import community.flock.wirespec.compiler.core.ModuleContent
-import community.flock.wirespec.compiler.core.ParseContext
 import community.flock.wirespec.compiler.core.ValidationContext
 import community.flock.wirespec.compiler.core.WirespecSpec
 import community.flock.wirespec.compiler.core.exceptions.DuplicateEndpointError
+import community.flock.wirespec.compiler.core.exceptions.WirespecException
 import community.flock.wirespec.compiler.core.parse
-import community.flock.wirespec.compiler.core.validate.Validator.validate
+import community.flock.wirespec.compiler.core.parse.AST
+import community.flock.wirespec.compiler.core.validate
 import community.flock.wirespec.compiler.utils.NoLogger
 import io.kotest.assertions.arrow.core.shouldBeLeft
-import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.matchers.types.shouldBeInstanceOf
 import kotlin.test.Test
 
 class ValidatorTest {
 
-    private fun parser(source1: String, source2: String) = object : ParseContext, NoLogger {
-        override val spec = WirespecSpec
-    }.parse(
-        nonEmptyListOf(
-            ModuleContent("module1.ws", source1),
-            ModuleContent("module2.ws", source2),
-        ),
-    )
+    private fun validate(vararg sources: String): EitherNel<WirespecException, AST> {
+        val context = object : ValidationContext, NoLogger {
+            override val spec = WirespecSpec
+        }
 
-    private val validator = object : ValidationContext, NoLogger {
-        override val spec = WirespecSpec
+        return context.validate(
+            context.parse(
+                sources.map { ModuleContent("module.ws", it) }.toNonEmptyListOrNull()
+                    ?: throw IndexOutOfBoundsException("No sources provided"),
+            ),
+        )
     }
 
     @Test
-    fun testDuplicateEndpointNamesAcrossModules() {
+    fun duplicateEndpointSameFile() {
+        val source = """
+            |endpoint Test GET /Test1 -> {
+            |    200 -> String
+            |}
+            |
+            |endpoint Test GET /Test2 -> {
+            |    200 -> String
+            |}
+        """.trimMargin()
+
+        validate(source)
+            .shouldBeLeft()
+            .head.shouldBeInstanceOf<DuplicateEndpointError>()
+    }
+
+    @Test
+    fun duplicateEndpointDifferentFile() {
         val source1 = """
             |endpoint Test GET /Test1 -> {
             |    200 -> String
@@ -43,30 +61,8 @@ class ValidatorTest {
             |}
         """.trimMargin()
 
-        val ast = parser(source1, source2)
-
-        validator.validate(ast)
+        validate(source1, source2)
             .shouldBeLeft()
             .head.shouldBeInstanceOf<DuplicateEndpointError>()
-    }
-
-    @Test
-    fun testNoDuplicateEndpointNamesAcrossModules() {
-        val source1 = """
-            |endpoint Test1 GET /Test1 -> {
-            |    200 -> String
-            |}
-        """.trimMargin()
-
-        val source2 = """
-            |endpoint Test2 GET /Test2 -> {
-            |    200 -> String
-            |}
-        """.trimMargin()
-
-        val ast = parser(source1, source2)
-
-        validator.validate(ast)
-            .shouldBeRight()
     }
 }
