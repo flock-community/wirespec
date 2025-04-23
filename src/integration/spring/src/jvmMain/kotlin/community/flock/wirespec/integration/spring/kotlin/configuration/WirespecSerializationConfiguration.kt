8ff9be522cb0efab0a50a7af8d5af9c958c3cbdb
Willem Veelenturf <willem.veelenturf@ing.com>
2025-04-23 08:42:17 +0200

    Remove fallback objectMapper bean

--- src/integration/spring/src/jvmMain/kotlin/community/flock/wirespec/integration/spring/kotlin/configuration/WirespecSerializationConfiguration.kt
+++ src/integration/spring/src/jvmMain/kotlin/community/flock/wirespec/integration/spring/kotlin/configuration/WirespecSerializationConfiguration.kt
@@ -6,7 +6,6 @@ import community.flock.wirespec.integration.spring.kotlin.web.WirespecResponseBo
 import community.flock.wirespec.kotlin.Wirespec.ParamSerialization
 import community.flock.wirespec.kotlin.Wirespec.Serialization
 import community.flock.wirespec.kotlin.serde.DefaultParamSerialization
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
@@ -14,10 +13,6 @@ import org.springframework.context.annotation.Import
 @Configuration
 @Import(WirespecResponseBodyAdvice::class, WirespecWebMvcConfiguration::class)
 open class WirespecSerializationConfiguration {
-
-    @Bean
-    @ConditionalOnMissingBean
-    open fun objectMapper(): ObjectMapper = ObjectMapper()
 
     @Bean
     open fun queryParamSerde(): ParamSerialization = DefaultParamSerialization()
