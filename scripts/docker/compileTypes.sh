bc22a2a06561c849f555f90d33fb0a40359e37a0
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2025-04-25 11:05:55 +0200

    fix: compile compiled wirespec correctly

--- scripts/docker/compileTypes.sh
+++ scripts/docker/compileTypes.sh
@@ -10,41 +10,44 @@ sharedPackageDir="community/flock/wirespec"
 platforms=("docker" "jvm" "native" "node")
 inputDirs=("openapi/petstore" "wirespec")
 
-printf "\nCompiling Kotlin Classes:\n"
-for platform in "${platforms[@]}"; do
-  for inputDir in "${inputDirs[@]}"; do
-    dir="/app/types/out/$platform/$inputDir/kotlin"
-    eval "kotlinc $dir/$compiledPackageDir/*.kt \
-     $dir/$sharedPackageDir/kotlin/*.kt \
-     -d $dir/wirespec.jar"
-  done
-done
+#printf "\nCompiling Kotlin Classes:\n"
+#for platform in "${platforms[@]}"; do
+#  for inputDir in "${inputDirs[@]}"; do
+#    dir="/app/types/out/$platform/$inputDir/kotlin"
+#    eval "kotlinc $dir/$compiledPackageDir/model/*.kt \
+#     $dir/$compiledPackageDir/endpoint/*.kt \
+#     $dir/$sharedPackageDir/*.kt \
+#     -d $dir/wirespec.jar"
+#  done
+#done
 
 printf "\nCompiling Java Classes:\n"
 for platform in "${platforms[@]}"; do
   for inputDir in "${inputDirs[@]}"; do
     dir="/app/types/out/$platform/$inputDir/java"
-    eval "javac $dir/$compiledPackageDir/*.java \
-     $dir/$sharedPackageDir/java/*.java \
+    eval "javac $dir/$compiledPackageDir/model/*.java \
+     $dir/$compiledPackageDir/endpoint/*.java \
+     $dir/$sharedPackageDir/*.java \
      -d $dir/wirespec.jar"
   done
 done
 
-printf "\nCompiling Scala Classes:\n"
-for platform in "${platforms[@]}"; do
-  for inputDir in "${inputDirs[@]}"; do
-    dir="/app/types/out/$platform/$inputDir/scala"
-    eval "scalac $dir/$compiledPackageDir/*.scala \
-     $dir/$sharedPackageDir/scala/*.scala \
-     -d $dir/wirespec.jar"
-  done
-done
-
-printf "\nCompiling TypeScript Classes:\n"
-for platform in "${platforms[@]}"; do
-  for inputDir in "${inputDirs[@]}"; do
-    dir="/app/types/out/$platform/$inputDir/typescript"
-    eval "tsc --noEmit $dir/$compiledPackageDir/*.ts \
-     $dir/$sharedPackageDir/typescript/*.ts"
-  done
-done
+#printf "\nCompiling Scala Classes:\n"
+#for platform in "${platforms[@]}"; do
+#  for inputDir in "${inputDirs[@]}"; do
+#    dir="/app/types/out/$platform/$inputDir/scala"
+#    eval "scalac $dir/$compiledPackageDir/model/*.scala \
+#     $dir/$compiledPackageDir/endpoint/*.scala \
+#     $dir/$sharedPackageDir/*.scala \
+#     -d $dir/wirespec.jar"
+#  done
+#done
+#
+#printf "\nCompiling TypeScript Classes:\n"
+#for platform in "${platforms[@]}"; do
+#  for inputDir in "${inputDirs[@]}"; do
+#    dir="/app/types/out/$platform/$inputDir/typescript"
+#    eval "tsc --noEmit $dir/$compiledPackageDir/*.ts \
+#     $dir/$sharedPackageDir/typescript/*.ts"
+#  done
+#done
