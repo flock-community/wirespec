ff681b7485d9638dff90545627b945c126be6561
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2025-05-01 08:38:18 +0200

    fix: combine output for type check

--- scripts/compileTypes.sh
+++ scripts/compileTypes.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+dir="$(dirname -- "$0")"
+root="$dir/.."
+
+output="$root/types/out"
+
 archSpecific=""
 if [[ $(uname -m) = arm64 ]]; then
   archSpecific="--platform=linux/amd64"
 fi
 
+diff -qr "$output/docker/" "$output/jvm/" --exclude='*.jar' && \
+diff -qr "$output/jvm/" "$output/native/" --exclude='*.jar' && \
+diff -qr "$output/native/" "$output/node/" --exclude='*.jar' && \
+cp -r "$output/jvm/." "$output/combined" && \
 docker run $archSpecific --rm -it -v ./types/:/app/types wirespec /app/compileTypes.sh
