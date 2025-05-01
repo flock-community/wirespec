27a90a6539238514964d29c41c2fcd42707a01ad
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2025-05-01 11:18:47 +0200

    fix: combine output for type check (#472)

--- scripts/docker/compileTypes.sh
+++ scripts/docker/compileTypes.sh
@@ -4,41 +4,31 @@
 
 source /root/.bashrc
 
-compiledPackageDir="community/flock/generated"
-sharedPackageDir="community/flock/wirespec"
+combinedOutputDir="/app/types/out/combined/"
 
-platforms=("docker" "jvm" "native" "node")
 inputDirs=("openapi/petstore" "wirespec")
 
 printf "\nCompiling Kotlin Classes:\n"
-for platform in "${platforms[@]}"; do
-  for inputDir in "${inputDirs[@]}"; do
-    dir="/app/types/out/$platform/$inputDir/kotlin"
-    find "$dir" -name '*.kt' -print0 | xargs -0 kotlinc -d "$dir/wirespec.jar"
-  done
+for inputDir in "${inputDirs[@]}"; do
+  dir="$combinedOutputDir$inputDir/kotlin"
+  find "$dir" -name '*.kt' -print0 | xargs -0 kotlinc -d "$dir/wirespec.jar"
 done
 
 printf "\nCompiling Java Classes:\n"
-for platform in "${platforms[@]}"; do
-  for inputDir in "${inputDirs[@]}"; do
-    dir="/app/types/out/$platform/$inputDir/java"
-    find "$dir" -name '*.java' -print0 | xargs -0 javac -d "$dir/target"
-    eval "jar cvf $dir/wirespec.jar $dir/target/*"
-  done
+for inputDir in "${inputDirs[@]}"; do
+  dir="$combinedOutputDir$inputDir/java"
+  find "$dir" -name '*.java' -print0 | xargs -0 javac -d "$dir/target"
+  eval "jar cvf $dir/wirespec.jar $dir/target/*"
 done
 
 printf "\nType checking Python files:\n"
-for platform in "${platforms[@]}"; do
-  for inputDir in "${inputDirs[@]}"; do
-    dir="/app/types/out/$platform/$inputDir/python"
-    find "$dir" -name '*.py' -print0 | xargs -0 python3 -m mypy
-  done
+for inputDir in "${inputDirs[@]}"; do
+  dir="$combinedOutputDir$inputDir/python"
+  find "$dir" -name '*.py' -print0 | xargs -0 python3 -m mypy
 done
 
 printf "\nType checking TypeScript files:\n"
-for platform in "${platforms[@]}"; do
-  for inputDir in "${inputDirs[@]}"; do
-    dir="/app/types/out/$platform/$inputDir/typescript"
-    find "$dir" -name '*.ts' -print0 | xargs -0 tsc --noEmit
-  done
+for inputDir in "${inputDirs[@]}"; do
+  dir="$combinedOutputDir$inputDir/typescript"
+  find "$dir" -name '*.ts' -print0 | xargs -0 tsc --noEmit
 done
