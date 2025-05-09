4964e368a16f62d140b324c8ced728a08eae9150
Willem Veelenturf <willem.veelenturf@ing.com>
2025-05-09 20:59:36 +0200

    fix: update publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,8 +79,8 @@ subprojects {
             repositories {
                 maven {
                     name = "oss"
-                    val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-                    val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+                    val releasesRepoUrl = uri("https://ossrh-staging-api.central.sonatype.com/service/local/")
+                    val snapshotsRepoUrl = uri("https://central.sonatype.com/repository/maven-snapshots/")
                     url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
                     credentials {
                         username = System.getenv("SONATYPE_USERNAME")
