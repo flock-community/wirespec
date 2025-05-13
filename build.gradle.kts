37ba3e0226f77ddb7aa8e332f177d1bc9437128e
Willem Veelenturf <willem.veelenturf@ing.com>
2025-05-13 14:13:02 +0200

    Apply nexus publish plugin conditionally in build script
    The nexus publish plugin is now marked with `apply false` to avoid it being applied automatically. This change ensures more explicit plugin application where needed, improving build script clarity and control.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.spotless)
-    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0" apply false
     `maven-publish`
     signing
 }
