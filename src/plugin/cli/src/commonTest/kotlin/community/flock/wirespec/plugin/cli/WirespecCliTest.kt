a938ad912bb2e3691ef1bff0c0c791b12f6b44cf
Willem Veelenturf <willem.veelenturf@ing.com>
2025-05-22 17:14:36 +0200

    Fix test

--- src/plugin/cli/src/commonTest/kotlin/community/flock/wirespec/plugin/cli/WirespecCliTest.kt
+++ src/plugin/cli/src/commonTest/kotlin/community/flock/wirespec/plugin/cli/WirespecCliTest.kt
@@ -151,10 +151,25 @@ class WirespecCliTest {
             ),
         )
 
-        val directoryPath = DirectoryPath(output)
-        val path = FilePath(directoryPath, Name("Petstore"), FileExtension.TypeScript)
+        val directoryPath = DirectoryPath("${output}/model")
+        val path = FilePath(directoryPath,  Name("Pet"), FileExtension.TypeScript)
 
-        path.read() shouldContain """
+        path.read() shouldBe  """
+            |export namespace Wirespec {
+            |  export type Method = "GET" | "PUT" | "POST" | "DELETE" | "OPTIONS" | "HEAD" | "PATCH" | "TRACE"
+            |  export type RawRequest = { method: Method, path: string[], queries: Record<string, string>, headers: Record<string, string>, body?: string }
+            |  export type RawResponse = { status: number, headers: Record<string, string>, body?: string }
+            |  export type Request<T> = { path: Record<string, unknown>, method: Method, queries?: Record<string, unknown>, headers?: Record<string, unknown>, body?:T }
+            |  export type Response<T> = { status:number, headers?: Record<string, unknown>, body?:T }
+            |  export type Serialization = { serialize: <T>(type: T) => string; deserialize: <T>(raw: string | undefined) => T }
+            |  export type Client<REQ extends Request<unknown>, RES extends Response<unknown>> = (serialization: Serialization) => { to: (request: REQ) => RawRequest; from: (response: RawResponse) => RES }
+            |  export type Server<REQ extends Request<unknown>, RES extends Response<unknown>> = (serialization: Serialization) => { from: (request: RawRequest) => REQ; to: (response: RES) => RawResponse }
+            |  export type Api<REQ extends Request<unknown>, RES extends Response<unknown>> = { name: string; method: Method, path: string, client: Client<REQ, RES>; server: Server<REQ, RES> }
+            |}
+            |
+            |import {Category} from '../model/Category'
+            |import {Tag} from '../model/Tag'
+            |import {PetStatus} from '../model/PetStatus'
             |export type Pet = {
             |  "id": number | undefined,
             |  "category": Category | undefined,
@@ -163,6 +178,7 @@ class WirespecCliTest {
             |  "tags": Tag[] | undefined,
             |  "status": PetStatus | undefined
             |}
+            |
         """.trimMargin()
     }
 
