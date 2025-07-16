f31253f0f713bebae683f1eee4aa02597676a3aa
Willem Veelenturf <willem.veelenturf@ing.com>
2025-07-16 10:02:11 +0200

    Clean up

--- src/plugin/npm/src/jsMain/resources/wirespec-fetch.d.ts
+++ src/plugin/npm/src/jsMain/resources/wirespec-fetch.d.ts
@@ -1,5 +1,3 @@
-export declare const hello: string
-
 export type Method = "GET" | "PUT" | "POST" | "DELETE" | "OPTIONS" | "HEAD" | "PATCH" | "TRACE"
 export type RawRequest = { method: Method, path: string[], queries: Record<string, string>, headers: Record<string, string>, body?: string }
 export type RawResponse = { status: number, headers: Record<string, string>, body?: string }
