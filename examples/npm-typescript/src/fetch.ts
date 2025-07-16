6b3135fc23a25e24682c4ef36ce57b2ff2ab6fd8
Willem Veelenturf <willem.veelenturf@ing.com>
2025-07-16 10:00:34 +0200

    Add TypeScript fetch example and update npm plugin exports
    Introduced a new TypeScript example demonstrating fetch functionality. Extended npm plugin configuration to include fetch-related exports and typings. Enhanced testing scripts to include fetch example validation.

---
+++ examples/npm-typescript/src/fetch.ts
@@ -0,0 +1,37 @@
+import * as assert from "node:assert";
+
+// @ts-ignore
+const handler = async (path, init)=> {
+    assert.equal(path, "test?test=test")
+    assert.deepEqual(init.headers, {"test":"TEST"})
+    return Promise.resolve({
+        headers: new Map([
+            ["Content-Type", "application/json"],
+            ["Content-Length", "2"],
+        ]),
+        text: () => Promise.resolve("{}")
+    })
+}
+
+const req = {
+    method: "GET",
+    path: ["test"],
+    queries: {
+        "test": "test"
+    },
+    headers: {
+        "test": "TEST"
+    },
+    body: "",
+}
+
+const fetch = async () => {
+    const { wirespecFetch } = await import("wirespec/fetch");
+    // @ts-ignore
+    const res = await wirespecFetch(req, handler)
+    console.log(res)
+    assert.equal(res.body, "{}")
+}
+
+fetch()
+
---
+++ src/plugin/npm/src/jsMain/resources/wirespec-fetch.mjs
@@ -0,0 +1,30 @@
+export async function wirespecFetch(req, handler) {
+    const contentHeader = req.body ? { 'Content-Type': 'application/json' } : {};
+    const body = req.body !== undefined ? req.body : undefined;
+    const query = Object.entries(req.queries)
+        .filter(([_, value]) => value !== undefined)
+        .flatMap(([key, value]) => {
+            if (value && typeof value === 'string' && value.startsWith('[') && value.endsWith(']')) {
+                const parsedValue = JSON.parse(value);
+                if (Array.isArray(parsedValue)) {
+                    return parsedValue.map((item) => `${key}=${item}`);
+                }
+            }
+            return `${key}=${value}`;
+        })
+        .join('&');
+    const url = `${req.path.join('/')}${query ? `?${query}` : ''}`;
+    const init = {method: req.method, body, headers: {...req.headers, ...contentHeader}}
+    const res = handler ? await handler(url, init) : await fetch(url, init)
+    const contentType = res.headers.get('Content-Type');
+    const contentLength = res.headers.get('Content-Length');
+    return {
+        status: res.status,
+        headers: {
+            ...headers,
+            'Content-Type': contentType,
+        },
+        body: contentLength !== '0' && contentType ? await res.text() : undefined,
+    };
+
+}
