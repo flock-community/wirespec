ae95f9146e9fff61da2209acf746887e6e16dccf
Willem Veelenturf <willem.veelenturf@ing.com>
2025-07-17 11:38:22 +0200

    Fix example implementation

--- examples/maven-spring-compile/src/main/java/community/flock/wirespec/example/maven/custom/app/user/UserConverter.java
+++ examples/maven-spring-compile/src/main/java/community/flock/wirespec/example/maven/custom/app/user/UserConverter.java
@@ -1,6 +1,8 @@
 package community.flock.wirespec.example.maven.custom.app.user;
 
 import community.flock.wirespec.example.maven.custom.app.common.Converter;
+import community.flock.wirespec.generated.java.model.AuthenticationPassword;
+import community.flock.wirespec.generated.java.model.AuthenticationToken;
 import community.flock.wirespec.generated.java.model.UserDto;
 import org.springframework.stereotype.Component;
 
@@ -8,11 +10,16 @@ import org.springframework.stereotype.Component;
 public class UserConverter implements Converter<User, UserDto> {
     @Override
     public User internalize(final UserDto userDto) {
-        return new User(userDto.name());
+        return switch (userDto.authentication()) {
+            case AuthenticationPassword password -> new User(userDto.name(), password.secret());
+            case AuthenticationToken token -> throw new UnsupportedOperationException("Token authentication not yet supported.");
+            case null -> throw new IllegalStateException("Authentication cannot be null");
+        };
     }
+
 
     @Override
     public UserDto externalize(final User user) {
-        return new UserDto(user.name());
+        return new UserDto(user.name(), new AuthenticationPassword(user.password()));
     }
 }
