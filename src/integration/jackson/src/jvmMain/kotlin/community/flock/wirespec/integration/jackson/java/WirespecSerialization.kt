ad7e9ca4de8e32750adacb6f560af6a008b842e6
wilmveel <willem.veelenturf@gmail.com>
2025-08-16 15:04:03 +0200

    Remove unused `ContentType` import from `WirespecSerialization.kt`

--- src/integration/jackson/src/jvmMain/kotlin/community/flock/wirespec/integration/jackson/java/WirespecSerialization.kt
+++ src/integration/jackson/src/jvmMain/kotlin/community/flock/wirespec/integration/jackson/java/WirespecSerialization.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import community.flock.wirespec.java.Wirespec.ParamSerialization
 import community.flock.wirespec.java.Wirespec.Serialization
 import community.flock.wirespec.java.serde.DefaultParamSerialization
-import jdk.jfr.ContentType
 import java.lang.reflect.Type
 
 /**
