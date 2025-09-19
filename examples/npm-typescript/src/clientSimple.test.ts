d316c7a63f5b2d272173a81c7fc195e44fbc8862
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2025-09-19 11:21:55 +0200

    chore(format): format files according to prettier code formatter
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- examples/npm-typescript/src/clientSimple.test.ts
+++ examples/npm-typescript/src/clientSimple.test.ts
@@ -4,66 +4,92 @@ import { test, expect } from "vitest";
 import { wirespecSerialization } from 'wirespec/serialization'
 
 const body = [
-    { id: "1", name: "Do it now", done: true },
-    { id: "2", name: "Do it tomorrow", done: false }
+  { id: "1", name: "Do it now", done: true },
+  { id: "2", name: "Do it tomorrow", done: false },
 ];
 
-type Api =
-    GetTodos.Handler &
-    GetTodoById.Handler &
-    PostTodo.Handler
+type Api = GetTodos.Handler & GetTodoById.Handler & PostTodo.Handler;
 
-const handleFetch = <Req extends Wirespec.Request<any>, Res extends Wirespec.Response<any>>(client: Wirespec.Client<Req, Res>) => (request: Req): Promise<Res> => {
-    const mock = (method: Wirespec.Method, path: string[], status: number, headers: Record<string, string>, body: any) => ({
-        method,
-        path,
-        status,
-        headers,
-        body
+const handleFetch =
+  <Req extends Wirespec.Request<any>, Res extends Wirespec.Response<any>>(
+    client: Wirespec.Client<Req, Res>
+  ) =>
+  (request: Req): Promise<Res> => {
+    const mock = (
+      method: Wirespec.Method,
+      path: string[],
+      status: number,
+      headers: Record<string, string>,
+      body: any
+    ) => ({
+      method,
+      path,
+      status,
+      headers,
+      body,
     });
     const mocks = [
-        mock("GET", ["api", "todos"], 200, {"x-total":"2"}, JSON.stringify(body)),
-        mock("GET", ["api", "todos", "1"], 200, {}, JSON.stringify(body[0])),
-        mock("POST", ["api", "todos"], 200, {}, JSON.stringify({ id: "3", name: "Do more", done: true }))
+      mock(
+        "GET",
+        ["api", "todos"],
+        200,
+        { "x-total": "2" },
+        JSON.stringify(body)
+      ),
+      mock("GET", ["api", "todos", "1"], 200, {}, JSON.stringify(body[0])),
+      mock(
+        "POST",
+        ["api", "todos"],
+        200,
+        {},
+        JSON.stringify({ id: "3", name: "Do more", done: true })
+      ),
     ];
     const rawRequest = client(wirespecSerialization).to(request);
-    const rawResponse = mocks.find(it =>
+    const rawResponse = mocks.find(
+      (it) =>
         it.method === rawRequest.method &&
         it.path.join("/") === rawRequest.path.join("/")
     );
-    if(rawResponse == undefined) throw new Error("Request is undefined")
+    if (rawResponse == undefined) throw new Error("Request is undefined");
+
     return Promise.resolve(client(wirespecSerialization).from(rawResponse));
-};
+  };
 
 const api: Api = {
-    postTodo: handleFetch(PostTodo.client),
-    getTodos: handleFetch(GetTodos.client),
-    getTodoById: handleFetch(GetTodoById.client)
+  postTodo: handleFetch(PostTodo.client),
+  getTodos: handleFetch(GetTodos.client),
+  getTodoById: handleFetch(GetTodoById.client),
 };
 
-test('testGetTodos', async () => {
-    const request: GetTodos.Request = GetTodos.request({done:undefined});
-    const response = await api.getTodos(request);
-    const expected = { status: 200, headers: {"X-Total":"2"}, body };
-    expect(response).toEqual(expected)
-})
+test("testGetTodos", async () => {
+  const request: GetTodos.Request = GetTodos.request({ done: undefined });
+  const response = await api.getTodos(request);
+  const expected = { status: 200, headers: { "X-Total": "2" }, body };
 
-test('testGetTodoById', async () => {
-    const request: GetTodoById.Request = GetTodoById.request({ id: "1" });
-    const response = await api.getTodoById(request);
-    const expected = GetTodoById.response200({ body: body[0] });
-    expect(response).toEqual(expected)
-})
+  expect(response).toEqual(expected);
+});
 
-test('testPostTodo', async () => {
-    const request: PostTodo.Request = {
-        method: "POST",
-        path: {},
-        queries: {},
-        headers: {},
-        body: { name: "Do more", done: true }
-    };
-    const response = await api.postTodo(request);
-    const expected = PostTodo.response200({ body: { id: "3", name: "Do more", done: true } });
-    expect(response).toEqual(expected)
-})
+test("testGetTodoById", async () => {
+  const request: GetTodoById.Request = GetTodoById.request({ id: "1" });
+  const response = await api.getTodoById(request);
+  const expected = GetTodoById.response200({ body: body[0] });
+
+  expect(response).toEqual(expected);
+});
+
+test("testPostTodo", async () => {
+  const request: PostTodo.Request = {
+    method: "POST",
+    path: {},
+    queries: {},
+    headers: {},
+    body: { name: "Do more", done: true },
+  };
+  const response = await api.postTodo(request);
+  const expected = PostTodo.response200({
+    body: { id: "3", name: "Do more", done: true },
+  });
+
+  expect(response).toEqual(expected);
+});
