9d6e73c16b36ea9b80895e7679c0278f9547c2bb
Julius van Dis <julius.van.dis@flock.community>
2026-01-26 20:46:32 +0100

    Add AvroEmitter tests

--- src/compiler/test/src/commonMain/kotlin/community/flock/wirespec/compiler/test/TestUtils.kt
+++ src/compiler/test/src/commonMain/kotlin/community/flock/wirespec/compiler/test/TestUtils.kt
@@ -7,10 +7,10 @@ import community.flock.wirespec.compiler.core.FileUri
 import community.flock.wirespec.compiler.core.ModuleContent
 import community.flock.wirespec.compiler.core.WirespecSpec
 import community.flock.wirespec.compiler.core.compile
-import community.flock.wirespec.compiler.core.emit.LanguageEmitter
+import community.flock.wirespec.compiler.core.emit.Emitter
 import community.flock.wirespec.compiler.utils.NoLogger
 
-fun compile(source: String) = { emitter: () -> LanguageEmitter ->
+fun compile(source: String) = { emitter: () -> Emitter ->
     object : CompilationContext, NoLogger {
         override val spec = WirespecSpec
         override val emitters = nonEmptySetOf(emitter())
