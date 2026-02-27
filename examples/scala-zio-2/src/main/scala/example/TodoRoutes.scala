d718f1e329aa2ac5b71ce52d9d205f58e8ce963e
Paul Hofma <paul_hofma@hotmail.com>
2026-02-27 14:25:11 +0100

    claude wip

--- examples/scala-zio-2/src/main/scala/example/TodoRoutes.scala
+++ examples/scala-zio-2/src/main/scala/example/TodoRoutes.scala
@@ -1,27 +1,15 @@
 package example
 
 import community.flock.wirespec.generated.endpoint.GetTodos
-import example.ZIOHttpFromWirespec.*
+import community.flock.wirespec.scala.Wirespec
 import zio.*
 import zio.http.*
-import zio.http.codec.PathCodec
 
 object TodoRoutes:
-  def routes(): Routes[GetTodos.Handler[List], Nothing] =
+  def routes(serialization: Wirespec.Serialization): Routes[GetTodos.Handler[Task], Throwable] =
     Routes(
-      GetTodos.Server.zioPath -> handler {
-          ZIO.serviceWith[GetTodos.Handler[List]](_.getTodos).map()
-      },
-//      Method.POST / "todos" -> handler {
-//        ZIO.succeed(Response.text("Create a new todo"))
-//      },
-//      Method.GET / "todos" / path[Int] -> handler { id =>
-//        ZIO.succeed(Response.text(s"Get todo with id: $id"))
-//      },
-//      Method.PUT / "todos" / path[Int] -> handler { id =>
-//        ZIO.succeed(Response.text(s"Update todo with id: $id"))
-//      },
-//      Method.DELETE / "todos" / path[Int] -> handler { id =>
-//        ZIO.succeed(Response.text(s"Delete todo with id: $id"))
-//      }
+      Method.GET / "api" / "todos" ->
+        ZIOHttpFromWirespec.handle(serialization, GetTodos.Server) { req =>
+          ZIO.serviceWithZIO[GetTodos.Handler[Task]](_.getTodos(req))
+        }
     )
--- examples/scala-zio-2/src/main/scala/example/ZIOHttpFromWirespec.scala
+++ examples/scala-zio-2/src/main/scala/example/ZIOHttpFromWirespec.scala
@@ -1,18 +1,45 @@
 package example
 
 import community.flock.wirespec.scala.Wirespec
+import zio.*
 import zio.http.*
-import zio.http.codec.PathCodec
 
 object ZIOHttpFromWirespec {
-  extension [Req, Res](server: Wirespec.Server[Req, Res])
-    def zioPath: RoutePattern[Res] = Method.fromString(server.method) / PathCodec.path(server.pathTemplate)
 
-  extension [Req, Res](response: Wirespec.Response[Res])
-    def toZio: Response =
-      Response(
-        status = Status.fromInt(response.status), 
-        headers = response.headers.asInstanceOf[Response.Headers],
-        body = response.body.asInstanceOf[String]
+  private def fromZioRequest(request: Request): ZIO[Any, Throwable, Wirespec.RawRequest] =
+    request.body.asArray.map { bodyBytes =>
+      Wirespec.RawRequest(
+        method = request.method.name,
+        path = request.url.path.encode.stripPrefix("/").split("/").filter(_.nonEmpty).toList,
+        queries = request.url.queryParams.map.view.mapValues(_.toList).toMap,
+        headers = request.headers.map(h => h.headerName -> List(h.renderedValue)).toMap,
+        body = if (bodyBytes.nonEmpty) Some(bodyBytes) else None
       )
+    }
+
+  private def toZioResponse(raw: Wirespec.RawResponse): Response = {
+    val headerSeq = raw.headers.flatMap { case (k, vs) => vs.map(Header.Custom(k, _)) }.toSeq
+    Response(
+      status = Status.fromInt(raw.statusCode),
+      headers = Headers(headerSeq*),
+      body = raw.body.fold(Body.empty)(Body.fromArray)
+    )
+  }
+
+  def handle[R, Req <: Wirespec.Request[?], Res <: Wirespec.Response[?]](
+    serialization: Wirespec.Serialization,
+    server: Wirespec.Server[Req, Res]
+  )(
+    f: Req => ZIO[R, Throwable, Res]
+  ): Handler[R, Throwable, Request, Response] = {
+    val edge = server.server(serialization)
+    handler { (zioReq: Request) =>
+      for {
+        rawReq      <- fromZioRequest(zioReq)
+        wirespecReq  = edge.from(rawReq)
+        wirespecRes <- f(wirespecReq)
+        rawRes       = edge.to(wirespecRes)
+      } yield toZioResponse(rawRes)
+    }
+  }
 }
