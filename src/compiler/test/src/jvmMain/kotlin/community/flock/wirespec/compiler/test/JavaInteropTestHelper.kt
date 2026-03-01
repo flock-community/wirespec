873887c07ff8c0260d22b2fcff7064dda4da8f75
Willem Veelenturf <willem.veelenturf@flock.community>
2026-03-01 19:26:15 +0100

    Refactor: migrate `SpringJavaEmitterTest` from Kotlin to Java for improved Java interop and introduce `JavaInteropTestHelper`

--- src/compiler/test/src/jvmMain/kotlin/community/flock/wirespec/compiler/test/JavaInteropTestHelper.kt
+++ src/compiler/test/src/jvmMain/kotlin/community/flock/wirespec/compiler/test/JavaInteropTestHelper.kt
@@ -19,5 +19,6 @@ object JavaInteropTestHelper {
     }.parse(nonEmptyListOf(ModuleContent(FileUri(""), source))).getOrNull() ?: error("Parsing failed.")
 
     @JvmStatic
-    fun emit(emitter: Emitter, ast: Root, logger: Logger): List<Emitted> = emitter.emit(ast, logger)
+    fun emit(emitter: Emitter, ast: Root, logger: Logger): List<Emitted> =
+        emitter.emit(ast, logger)
 }
