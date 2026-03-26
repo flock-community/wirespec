2579fb4ad25eeb7979ca738e5ca537095073cac9
Willem Veelenturf <willem.veelenturf@flock.community>
2026-03-26 09:59:37 +0100

    Update `RawRequestIr` query type to align with header structure in wirespec-fetch typings

--- src/plugin/npm/src/jsMain/resources/wirespec-fetch.d.ts
+++ src/plugin/npm/src/jsMain/resources/wirespec-fetch.d.ts
@@ -1,6 +1,6 @@
 export type Method = "GET" | "PUT" | "POST" | "DELETE" | "OPTIONS" | "HEAD" | "PATCH" | "TRACE"
 export type RawRequest = { method: Method, path: string[], queries: Record<string, string>, headers: Record<string, string>, body?: string }
-export type RawRequestIr = { method: string, path: string[], queries: Record<string, string>, headers: Record<string, string[]>, body: Uint8Array | undefined }
+export type RawRequestIr = { method: string, path: string[], queries: Record<string, string[]>, headers: Record<string, string[]>, body: Uint8Array | undefined }
 export type RawResponse = { status: number, headers: Record<string, string>, body?: string }
 export type RawResponseIr = { statusCode:number, headers: Record<string, string[]>, body: Uint8Array | undefined }
 export type HandleFetch = ( path:string, init?:RequestInit) => Promise<Response>
