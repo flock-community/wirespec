06c6d314b2ccc7718c22a2bd93fbb0f1eb5f5ab6
Willem Veelenturf <willem.veelenturf@flock.community>
2026-04-23 12:05:30 +0200

    feat(ir): add subPackage-string overloads to placeInPackage/placeInModule

--- src/compiler/ir/src/commonMain/kotlin/community/flock/wirespec/ir/emit/EmitHelpers.kt
+++ src/compiler/ir/src/commonMain/kotlin/community/flock/wirespec/ir/emit/EmitHelpers.kt
@@ -3,6 +3,7 @@ package community.flock.wirespec.ir.emit
 import arrow.core.NonEmptyList
 import community.flock.wirespec.compiler.core.emit.EmitShared
 import community.flock.wirespec.compiler.core.emit.PackageName
+import community.flock.wirespec.compiler.core.emit.namespace
 import community.flock.wirespec.compiler.core.emit.plus
 import community.flock.wirespec.compiler.core.parse.ast.Definition
 import community.flock.wirespec.ir.core.Element
@@ -12,14 +13,19 @@ import community.flock.wirespec.ir.core.Package
 
 fun File.placeInPackage(
     packageName: PackageName,
-    definition: Definition,
+    subPackage: String,
 ): File {
-    val subPackageName = packageName + definition
+    val subPackageName = packageName + subPackage
     return File(
         name = Name.of(subPackageName.toDir() + name.pascalCase()),
         elements = listOf(Package(subPackageName.value)) + elements,
     )
 }
+
+fun File.placeInPackage(
+    packageName: PackageName,
+    definition: Definition,
+): File = placeInPackage(packageName, definition.namespace())
 
 fun File.prependImports(imports: List<Element>?): File = if (imports == null) {
     this
@@ -29,11 +35,16 @@ fun File.prependImports(imports: List<Element>?): File = if (imports == null) {
 
 fun File.placeInModule(
     packageName: PackageName,
-    definition: Definition,
+    subPackage: String,
 ): File {
-    val subPackageName = packageName + definition
+    val subPackageName = packageName + subPackage
     return copy(name = Name.of(subPackageName.toDir() + name.pascalCase()))
 }
+
+fun File.placeInModule(
+    packageName: PackageName,
+    definition: Definition,
+): File = placeInModule(packageName, definition.namespace())
 
 fun NonEmptyList<File>.withSharedSource(
     emitShared: EmitShared,
